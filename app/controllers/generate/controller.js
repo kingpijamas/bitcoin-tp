ab5e5b0d8e507e86435a5da9d4f414b72a7390e3
Teresa di Tada <tditada@gmail.com>
2016-11-13 20:42:45 -0300

    data generated

--- app/controllers/generate/controller.js
+++ app/controllers/generate/controller.js
@@ -5,6 +5,6 @@ var commons = require('../../commons.js');
 module.exports = function generateControllerController(req, res) {
     //var myService = commons.service("my");
     //myService(); // call the service
-    res.render('generate');
+    res.render('generate', {title:'data', link: 'myLink', json:'{lalal}'});
 
 };
